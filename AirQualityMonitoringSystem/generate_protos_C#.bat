64eabb8752460b0ab5a71fe66f2c9c264720edc1
Gabriel <gabi1998anton1998@gmail.com>
2019-09-20 18:39:17 +0300

    Added "air_quality.proto" file with a service for testing and generated the corresponding .cs files. Added a script that generates C# code from .proto files (.bat).

---
+++ AirQualityMonitoringSystem/generate_protos_C#.bat
@@ -0,0 +1,16 @@
+@rem Generate the code for C# .proto files
+
+setlocal
+
+set TOOLS_PATH=packages\Grpc.Tools.2.23.0\tools\windows_x64
+set PROTOS_PATH=Protos
+set OUTPUT_PATH=AirQuality\Generated
+
+if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
+
+for /R . %%f in (%PROTOS_PATH%\*.proto) do (
+	%TOOLS_PATH%\protoc.exe --proto_path %PROTOS_PATH% --csharp_out %OUTPUT_PATH% %PROTOS_PATH%\%%~nf%%~xf 
+	%TOOLS_PATH%\protoc.exe --proto_path %PROTOS_PATH% --grpc_out %OUTPUT_PATH% --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe %PROTOS_PATH%\%%~nf%%~xf
+)
+
+endlocal
